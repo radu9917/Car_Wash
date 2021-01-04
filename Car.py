f16d0a0ae7f9664caf1555a10a5d7697d37b2411
radu9917 <ion.radu17@yahoo.com>
2021-01-04 17:33:57 +0200

    Added getters and setters

--- Car.py
+++ Car.py
@@ -3,3 +3,23 @@ class Car:
         self.id = id
         self.number = number
         self.owner = owner
+
+    # Getters
+    def get_id(self):
+        return self.id
+
+    def get_number(self):
+        return self.number
+
+    def get_owner(self):
+        return self.owner
+
+    # Setters
+    def set_id(self, id):
+        self.id = id
+
+    def set_number(self, number):
+        self.number = number
+
+    def set_owner(self, owner):
+        self.owner = owner
