6bc16eacd2f44cbaf21b349be0c7e2913473052f
radu9917 <ion.radu17@yahoo.com>
2021-01-05 14:25:18 +0200

    update

--- main.py
+++ main.py
@@ -7,24 +7,39 @@ car_list = []
 
 def create_car():
 
-    print("Create car :")
-    index = input("Give car index:")
-    number = input("Give car number")
-    owner = input("Give car owner")
-    car_list.append(Car(index, number, owner))
+    nr = int(input("How many cars do you want to make?"))
+    for num in range(nr):
+        print("Create car number " + str(num+1))
+        index = int(input("Give car index:"))
+        number = input("Give car number")
+        owner = input("Give car owner")
+        car_list.append(Car(index, number, owner))
+
+
+def show_cars():
+    for car in car_list:
+        print(car)
 
 
 def delete_car():
     index = int(input("What car do you want to delete?"))
-    car_list.pop(index)
+    i = 0
+    for car in car_list:
+        if car.get_id() == index:
+            car_list.pop(i)
+        i += 1
 
 
 def modify_car():
     index = int(input("What car do you want to modify?"))
     owner = input("Give the owner's name")
     number = input("Give the car's number")
-    car_list[index].set_number(number)
-    car_list[index].set_owner(owner)
+    i = 0
+    for car in car_list:
+        if car.get_id() == index:
+            car_list[i].set_number(number)
+            car_list[i].set_owner(owner)
+        i += 1
 
 
 def view_all_car_wash():
@@ -34,31 +49,36 @@ def view_all_car_wash():
 
 def view_car_wash():
     choice = int(input("Choose a car wash"))
-    print(car_wash_list[choice])
+    i = 0
+    for car_wash in car_wash_list:
+        if choice == car_wash.get_id():
+            print(car_wash_list[i])
+        i += 1
 
 
 def rename_car_wash():
     choice = int(input("Choose a car wash to rename:"))
     name = input("Choose the name for the car wash")
-    car_wash_list[choice].set_name(name)
+    for car_wash in car_wash_list:
+        if choice == car_wash.get_id():
+            car_wash.set_name(name)
 
 
 def add_to_car_wash():
-    choice1 = input("Choose a car wash")
+    choice1 = int(input("Choose a car wash"))
     choice2 = int(input("Choose the car you want to be added"))
     for car_wash in car_wash_list:
         if choice1 == car_wash.get_id():
             for car in car_list:
-                if car.get_id() == choice2:
+                if choice2 == car.get_id():
                     car_wash.add_car(car)
 
 
 def remove_from_car_wash():
-    choice1 = input("Choose a car wash")
-    choice2 = int(input("Choose the car you want to be removed"))
+    choice1 = int(input("Choose a car wash"))
     for car_wash in car_wash_list:
         if choice1 == car_wash.get_id():
-            car_wash.remove_car(choice2)
+            car_wash.remove_car()
 
 
 def main():
@@ -74,9 +94,9 @@ def main():
     while True:
         print("Choose an option:\n1.Create car \n2.Delete car \n3.Modify car")
         print("4.View all car washes\n5.Rename car wash\n6.Add a car to a car wash")
-        print("7.Remove a car from a car wash\n8.View car wash")
+        print("7.Remove a car from a car wash\n8.View car wash\n9.Show cars")
         option = input()
-        options = {
+        menu = {
             "1": create_car,
             "2": delete_car,
             "3": modify_car,
@@ -84,9 +104,10 @@ def main():
             "5": rename_car_wash,
             "6": add_to_car_wash,
             "7": remove_from_car_wash,
-            "8": view_car_wash
+            "8": view_car_wash,
+            "9": show_cars
         }
-        options[option]()
+        menu[option]()
 
 
 main()
