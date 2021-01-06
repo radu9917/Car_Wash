ab8b8b15090c3062a6e128d399ab937fa51dfc56
radu9917 <ion.radu17@yahoo.com>
2021-01-06 15:38:09 +0200

    Created Tests

--- tests/test_car_wash_repo.py
+++ tests/test_car_wash_repo.py
@@ -8,7 +8,7 @@ def test_car_wash_repo():
     car_wash_list.store(car_wash)
     assert car_wash_list.get_all() is not None
     car_wash2 = CarWash(1, "Ian")
-    car_wash_list.update(1,car_wash2)
+    car_wash_list.update(1, car_wash2)
     assert car_wash_list.get_all()[0] == car_wash2
     car_wash_list.delete(1)
     car_wash_list.store(car_wash)
